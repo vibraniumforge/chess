51b8758856c3a6dcd1bd6ad2cf8659a6e1dc159a
github username <gregm9999@gmail.com>
2019-09-01 19:50:52 -0700

    added coords

--- src/components/Board.js
+++ src/components/Board.js
@@ -26,20 +26,21 @@ class Board extends Component {
     return (
       <React.Fragment>
         <table>
-          <thead>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-              <td>c</td>
-              <td>d</td>
-              <td>e</td>
-              <td>f</td>
-              <td>g</td>
-              <td>h</td>
-            </tr>
-          </thead>
           <tbody id="chess-board">
+            <tr className="border">
+              <td className="border"></td>
+              <td className="border">a</td>
+              <td className="border">b</td>
+              <td className="border">c</td>
+              <td className="border">d</td>
+              <td className="border">e</td>
+              <td className="border">f</td>
+              <td className="border">g</td>
+              <td className="border">h</td>
+              <td></td>
+            </tr>
             <tr>
+              <td className="border">8</td>
               <td id="a8" className="white-square"></td>
               <td id="b8" className="black-square "></td>
               <td id="c8" className="white-square"></td>
@@ -48,8 +49,10 @@ class Board extends Component {
               <td id="f8" className="black-square"></td>
               <td id="g8" className="white-square"></td>
               <td id="h8" className="black-square"></td>
+              <td className="border">8</td>
             </tr>
             <tr>
+              <td className="border">7</td>
               <td id="a7" className="black-square"></td>
               <td id="b7" className="white-square"></td>
               <td id="c7" className="black-square"></td>
@@ -58,8 +61,10 @@ class Board extends Component {
               <td id="f7" className="white-square"></td>
               <td id="g7" className="black-square"></td>
               <td id="h7" className="white-square"></td>
+              <td className="border">7</td>
             </tr>
             <tr>
+              <td className="border">6</td>
               <td id="a6" className="white-square"></td>
               <td id="b6" className="black-square"></td>
               <td id="c6" className="white-square"></td>
@@ -68,8 +73,10 @@ class Board extends Component {
               <td id="f6" className="black-square"></td>
               <td id="g6" className="white-square"></td>
               <td id="h6" className="black-square"></td>
+              <td className="border">6</td>
             </tr>
             <tr>
+              <td className="border">5</td>
               <td id="a5" className="black-square"></td>
               <td id="b5" className="white-square"></td>
               <td id="c5" className="black-square"></td>
@@ -78,8 +85,10 @@ class Board extends Component {
               <td id="f5" className="white-square"></td>
               <td id="g5" className="black-square"></td>
               <td id="h5" className="white-square"></td>
+              <td className="border">5</td>
             </tr>
             <tr>
+              <td className="border">4</td>
               <td id="a4" className="white-square"></td>
               <td id="b4" className="black-square"></td>
               <td id="c4" className="white-square"></td>
@@ -88,8 +97,10 @@ class Board extends Component {
               <td id="f4" className="black-square"></td>
               <td id="g4" className="white-square"></td>
               <td id="h4" className="black-square"></td>
+              <td className="border">4</td>
             </tr>
             <tr>
+              <td className="border">3</td>
               <td id="a3" className="black-square"></td>
               <td id="b3" className="white-square"></td>
               <td id="c3" className="black-square"></td>
@@ -98,8 +109,10 @@ class Board extends Component {
               <td id="f3" className="white-square"></td>
               <td id="g3" className="black-square"></td>
               <td id="h3" className="white-square"></td>
+              <td className="border">3</td>
             </tr>
             <tr>
+              <td className="border">2</td>
               <td id="a2" className="white-square"></td>
               <td id="b2" className="black-square"></td>
               <td id="c2" className="white-square"></td>
@@ -108,8 +121,10 @@ class Board extends Component {
               <td id="f2" className="black-square"></td>
               <td id="g2" className="white-square"></td>
               <td id="h2" className="black-square"></td>
+              <td className="border">2</td>
             </tr>
             <tr>
+              <td className="border">1</td>
               <td id="a1" className="black-square"></td>
               <td id="b1" className="white-square"></td>
               <td id="c1" className="black-square"></td>
@@ -118,20 +133,21 @@ class Board extends Component {
               <td id="f1" className="white-square"></td>
               <td id="g1" className="black-square"></td>
               <td id="h1" className="white-square"></td>
+              <td className="border">1</td>
+            </tr>
+            <tr>
+              <td className="border"></td>
+              <td className="border">a</td>
+              <td className="border">b</td>
+              <td className="border">c</td>
+              <td className="border">d</td>
+              <td className="border">e</td>
+              <td className="border">f</td>
+              <td className="border">g</td>
+              <td className="border">h</td>
+              <td className="border"></td>
             </tr>
           </tbody>
-          <tfoot>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-              <td>c</td>
-              <td>d</td>
-              <td>e</td>
-              <td>f</td>
-              <td>g</td>
-              <td>h</td>
-            </tr>
-          </tfoot>
         </table>
       </React.Fragment>
     );
